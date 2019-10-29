d9087fbabdf70d8865d989011a274e3f21433f04
svasquez <svasquez@controldiseno.com>
2019-10-29 10:58:32 -0500

    Edicion en generador de piers

--- Analisis muros concreto fc/Diseno muros concreto fc/Generador de piers.cs
+++ Analisis muros concreto fc/Diseno muros concreto fc/Generador de piers.cs
@@ -180,7 +180,7 @@ namespace Diseno_muros_concreto_fc
                     h_acumulado = Lista_Geometria[i].h_acumulado * 100,
                     Fc = Lista_Geometria[i].Fc / 10
                 };
-                Muro_i.Calc_pc();
+
 
                 List<Fuerza_Piers> Aux_fuerzas = Datos_Fuerza.FindAll(x => x.Pier == Muro_i.Pier & x.Story == Muro_i.Story);
                 for (int j = 0; j < Aux_fuerzas.Count; j++)
@@ -193,6 +193,7 @@ namespace Diseno_muros_concreto_fc
                     Muro_i.M2.Add(Aux_fuerzas[j].M2);
                     Muro_i.M3.Add(Aux_fuerzas[j].M3);
                 }
+                Muro_i.Calc_pc();
 
                 Muro_i.Shells_Muro = Listas_Programa.Lista_shells.FindAll(x => x.Pier == Muro_i.Pier & x.Story == Muro_i.Story);
                 List<Datos_Diseno> Aux_2 = Lista_Diseno.FindAll(x => x.Pier == Muro_i.Pier & x.Story == Muro_i.Story);
